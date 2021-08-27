df215152301cc02736ee44e25440caa1ea65b2df
pawel.puszka <pawel.puszka@gmail.com>
2021-08-27 15:15:40 +0200

    errors

--- src/main/java/arrays/matrix/Matrix.java
+++ src/main/java/arrays/matrix/Matrix.java
@@ -10,14 +10,14 @@ public class Matrix {
         matrixCalc.printMatrix(array);
 
         if (!CheckMatrix.isSymetric(array)) {
-            System.out.println("Macież nie jest symetryczna.");
-            System.out.println("Program nie obliczy przekątnych macieży.");
+            System.out.println("Macierz nie jest symetryczna.");
+            System.out.println("Program nie obliczy przekątnych macierzy.");
         }
         else {
-            System.out.println("Suma iloczynów przekątnych tablicy: " + matrixCalc.diagonalSum(array));
+            System.out.println("Suma iloczynów przekątnych macierzy: " + matrixCalc.diagonalSum(array));
         }
         System.out.println("Iloczyn zsumowanych elementów środkowego wiersza i środkowej kolumny: " + matrixCalc.crossMultiply(array));
-        System.out.println("Suma wszystkich elementów znajdujących się przy krawędzi tablicy: " + matrixCalc.boundSum(array));
+        System.out.println("Suma wszystkich elementów znajdujących się przy krawędzi macierzy: " + matrixCalc.boundSum(array));
 
         input.closeReading();
     }
