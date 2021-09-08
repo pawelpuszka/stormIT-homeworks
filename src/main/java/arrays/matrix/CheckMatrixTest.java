5e4e27a00768ed6a50a6a62d8ba0de4d2e18dda4
pawel.puszka <pawel.puszka@gmail.com>
2021-09-08 12:40:08 +0200

    Tests for CheckMatrix.isInRange method

--- src/main/java/arrays/matrix/CheckMatrixTest.java
+++ src/main/java/arrays/matrix/CheckMatrixTest.java
@@ -1,37 +1,88 @@
 package arrays.matrix;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import arrays.matrix.CheckMatrix;
 import org.junit.jupiter.api.*;
 
 class CheckMatrixTest {
 
     @Test
-    void isInRangeTest() {
-        boolean result1 = CheckMatrix.isInRange(3);
-        boolean result2 = CheckMatrix.isInRange(10);
-        boolean result3 = CheckMatrix.isInRange(5);
-        boolean result4 = CheckMatrix.isInRange(0);
-        boolean result5 = CheckMatrix.isInRange(11);
-        boolean result6 = CheckMatrix.isInRange(1000000000);
-        boolean result7 = CheckMatrix.isInRange(-100);
+    void isInRange_DimensionIsMinValue() {
+        int input = 3;
 
-        assertTrue(result1);
-        assertTrue(result2);
-        assertTrue(result3);
-        assertFalse(result4);
-        assertFalse(result5);
-        assertFalse(result6);
-        assertFalse(result7);
+        boolean result = CheckMatrix.isInRange(input);
+
+        assertTrue(result);
     }
 
     @Test
+    void isInRange_DimensionIsMaxValue() {
+        int input = 10;
+
+        boolean result = CheckMatrix.isInRange(input);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void isInRange_DimensionIsMidValue() {
+        int input = 5;
+
+        boolean result = CheckMatrix.isInRange(input);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void isInRange_DimensionIsApartFromLowerBound() {
+        int input = 2;
+
+        boolean result = CheckMatrix.isInRange(input);
+
+        assertFalse(result);
+    }
+
+    @Test
+    void isInRange_DimensionIsApartFromUpperBound() {
+        int input = 11;
+
+        boolean result = CheckMatrix.isInRange(input);
+
+        assertFalse(result);
+    }
+
+    @Test
+    void isInRange_DimensionIsZero() {
+        int input = 0;
+
+        boolean result = CheckMatrix.isInRange(input);
+
+        assertFalse(result);
+    }
+
+    @Test
+    void isInRange_DimensionIsExtremelyBig() {
+        int input = 1_000_000_000;
+
+        boolean result = CheckMatrix.isInRange(input);
+
+        assertFalse(result);
+    }
+
+    @Test
+    void isInRange_DimensionIsNegative() {
+        int input = -100;
+
+        boolean result = CheckMatrix.isInRange(input);
+
+        assertFalse(result);
+    }
+}
+
+   /* @Test
     void isSymmetricTest() {
         boolean result1 = CheckMatrix.isSymmetric(new double[5][5]);
         boolean result2 = CheckMatrix.isSymmetric(new double[3][6]);
         boolean result3 = CheckMatrix.isSymmetric(new double[1][2]);
-        boolean isInRangeResult = CheckMatrix.isInRange()
 
         assertTrue(result1);
         assertFalse(result2);
@@ -40,5 +91,4 @@ class CheckMatrixTest {
 
     @Test
     void isNull() {
-    }
-}
+    }*/
